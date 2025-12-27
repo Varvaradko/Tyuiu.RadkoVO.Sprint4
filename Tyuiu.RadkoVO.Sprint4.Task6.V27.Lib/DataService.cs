a5019b583160649978a969c81750fc47967dc8bf
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-12-27 11:39:33 +0500

    Task6

---
+++ Tyuiu.RadkoVO.Sprint4.Task6.V27.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.RadkoVO.Sprint4.Task6.V27.Lib
+{
+    public class DataService : ISprint4Task6V27
+    {
+        public int Calculate(string[] array)
+        {
+            string[] mas = Array.FindAll(array, x => x.Length < 7);
+            return mas.Length;
+        }
+    }
+}
---
+++ Tyuiu.RadkoVO.Sprint4.Task6.V27.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.RadkoVO.Sprint4.Task6.V27.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint4.Task6.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            var shapes = new string[]
+            {
+                "Квадрат", "Прямоугольник", "Круг", "Треугольник",
+                "Пятиугольник", "Шестиугольник", "Восьмиугольник"
+            };
+
+            int res = ds.Calculate(shapes);
+            int wait = 1; // "Квадрат" (6 символов) и "Круг" (4 символа) - меньше 7
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
